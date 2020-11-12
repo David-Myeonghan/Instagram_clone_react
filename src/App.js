385e3fcc85bad4cb09adef4fa34f660f6fa523e1
David MH Ryu <myeonghan12@gmail.com>
2020-11-12 23:39:56 +1000

    modal for sign up & firebase createUser

--- src/App.js
+++ src/App.js
@@ -1,30 +1,112 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
-import { db } from "./firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { db, auth } from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button, Input } from "@material-ui/core";
+
+function getModalStyle() {
+	const top = 50;
+	const left = 50;
+
+	return {
+		top: `${top}%`,
+		left: `${left}%`,
+		transform: `translate(-${top}%, -${left}%)`,
+	};
+}
+
+const useStyles = makeStyles((theme) => ({
+	paper: {
+		position: "absolute",
+		width: 400,
+		backgroundColor: theme.palette.background.paper,
+		border: "2px solid #000",
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(2, 4, 3),
+	},
+}));
 
 function App() {
-	const [posts, setPosts] = useState([
-		{
-			username: "JS",
-			caption: "cloning",
-			imageUrl:
-				"https://images.unsplash.com/photo-1605041176777-23275b7a1410?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=954&q=80",
-		},
-		{
-			username: "Insta",
-			caption: "1 gram",
-			imageUrl:
-				"https://images.unsplash.com/photo-1605033224723-0277b1e7b995?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
-		},
-	]);
+	const classes = useStyles();
+	const [modalStyle] = useState(getModalStyle);
+
+	const [posts, setPosts] = useState([]);
+	const [open, setOpen] = useState(false);
+
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const [user, setUser] = useState(null);
 
 	useEffect(() => {
-		db.collection("posts");
+		auth.onAuthStateChanged((authUser) => {
+			if (authUser) {
+				// ..user has logged in...
+				console.log(authUser);
+				setUser(authUser);
+			} else {
+				// user has logged out.
+				setUser(null);
+			}
+		});
+	}, [user, username]);
+
+	useEffect(() => {
+		db.collection("posts").onSnapshot((snapshot) => {
+			//every time a new post is added, this code fires.
+			setPosts(
+				snapshot.docs.map((doc) => ({
+					id: doc.id,
+					post: doc.data(),
+				}))
+			);
+		});
 	}, []);
+
+	const signUp = (event) => {
+		event.preventDefault();
+		auth
+			.createUserWithEmailAndPassword(email, password)
+			.then((authUser) => {
+				return authUser.user.updateProfile({
+					displayName: username,
+				});
+			})
+			.catch((error) => alert(error.message));
+	};
 
 	return (
 		<div className="app">
+			<Modal open={open} onClose={() => setOpen(false)}>
+				<div style={modalStyle} className={classes.paper}>
+					<form className="app__signup">
+						<center>
+							<img
+								className="app__headerImage"
+								src="https://instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+								alt=""
+							/>
+						</center>
+						<Input placeholder="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+						<Input placeholder="email" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+						<Input
+							placeholder="password"
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+						<Button type="submit" onClick={signUp}>
+							Sign Up
+						</Button>
+					</form>
+				</div>
+			</Modal>
+
 			<div className="app__header">
 				<img
 					className="app__headerImage"
@@ -32,10 +114,12 @@ function App() {
 					alt=""
 				></img>
 			</div>
+
+			<Button onClick={() => setOpen(true)}>Sign Up</Button>
 			<h1>Instagram clone</h1>
 
-			{posts.map((post) => (
-				<Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+			{posts.map(({ id, post }) => (
+				<Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
 			))}
 
 			{/* <Post
