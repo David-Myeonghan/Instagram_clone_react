fcaa7fdaffb62a136ba0c2be7cee7a819bad685f
David MH Ryu <myeonghan12@gmail.com>
2020-11-14 00:00:03 +1000

    sign in & out

--- src/App.js
+++ src/App.js
@@ -79,10 +79,15 @@ function App() {
 				});
 			})
 			.catch((error) => alert(error.message));
+
+		setOpen(false);
 	};
 
 	const signIn = (event) => {
 		event.preventDefault();
+
+		auth.signInWithEmailAndPassword(email, password).catch((error) => alert(error.message));
+		setOpenSignIn(false);
 	};
 
 	return (
